ba6afbf09a4342e15054f0277a1ce1c7e725b0ba
atul <atul.kumar@appinventiv.com>
2020-03-27 12:39:27 +0530

    removed build folder

--- build/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fib_controller_1 = require("./fib/fib.controller");
-exports.fibController = fib_controller_1.fibController;
-//# sourceMappingURL=index.js.map
--- build/rabbitmq/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var connection_1 = require("./connection");
-exports.rabbitMq = connection_1.rabbitMq;
-var consumer_1 = require("./consumer");
-exports.rabbitMqConsumer = consumer_1.rabbitMqConsumer;
-var publisher_1 = require("./publisher");
-exports.rabbitMqProducer = publisher_1.rabbitMqProducer;
-//# sourceMappingURL=index.js.map
